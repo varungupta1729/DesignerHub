b80266c266e7326d17edf4dd97e577a51a6d2c65
aashishgupta49 <94563179+aashishgupta49@users.noreply.github.com>
2023-06-20 02:13:34 +0530

    profile card added

---
+++ frontend/src/components/profile_card/profile.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import './profile.css';
+
+
+function Profile() {
+  return (
+    
+
+  <body>
+    <div class="container">
+      <div class="box">
+      <div class="image">
+        <img src="img1.jpeg"/>
+        </div>
+        <div class="name_job">David Chrish</div>
+        <div class="rating">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="far fa-star"></i>
+          <i class="far fa-star"></i>
+          <i class="far fa-star"></i>
+        </div>
+        <p> Lorem ipsum dolor sitamet, stphen hawkin so adipisicing elit. Ratione disuja doloremque reiciendi nemo.</p>
+        <div class="btns">
+          <button>Read More</button>
+          <button>Subscribe</button>
+        </div>
+      </div>
+      <div class="box">
+        <div class="image">
+        <img src="img2.jpeg" alt=""/>
+        </div>
+        <div class="name_job">Kristina Bellis</div>
+        <div class="rating">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="far fa-star"></i>
+          <i class="far fa-star"></i>
+        </div>
+        <p> Lorem ipsum dolor sitamet, stphen hawkin so adipisicing elit. Ratione disuja doloremque reiciendi nemo.</p>
+        <div class="btns">
+          <button>Read More </button>
+          <button>Subscribe</button>
+        </div>
+      </div>
+      <div class="box">
+        <div class="image">
+         <img src="img3.jpeg" alt=""/>
+        </div>
+        <div class="name_job">Stephen Marlo</div>
+          <div class="rating">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="far fa-star"></i>
+        </div>
+        <p> Lorem ipsum dolor sitamet, stphen hawkin so adipisicing elit. Ratione disuja doloremque reiciendi nemo.</p>
+        <div class="btns">
+          <button>Read More</button>
+          <button>Subscribe</button>
+        </div>
+      </div>
+    </div>
+      </body>
+
+  );
+}
+
+export default Profile;
