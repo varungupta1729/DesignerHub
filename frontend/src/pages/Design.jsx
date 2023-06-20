b8a5b5a8482474b8cd038765c2fc05873d122aff
ayushsachan123 <ayushsachan.in@gmail.com>
2023-06-20 22:57:28 +0530

    add Design.jsx

---
+++ frontend/src/pages/Design.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import App from '../components/TshirtDesign/App'
+const Design = () => {
+  return (
+    <div>
+    <App/>
+    </div>
+  )
+}
+
+export default Design
+
